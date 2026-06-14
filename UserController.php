5d6b194ea7f71c129420e223cab1c3979eb51a77
taisei <tsukuda12085735@gmail.com>
2026-06-15 08:33:56 +0900

    Fix review comments: Add validation and hash password

--- UserController.php
+++ UserController.php
@@ -4,22 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::latest()->pagenate(20);
         return view('users.index', ['users' => $users]);
     }
 
     public function store(Request $request)
     {
-        $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save();
+        $validated = $request->validate([
+            'name' => 'required|string|max:50',
+            'email' => 'required|email|Unique:users',
+            'password' => 'required|min:8'
+        ]);
+
+        User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
+        ]);
 
         return redirect('/users');
     }
